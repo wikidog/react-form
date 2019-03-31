8b0eac51ee157b9d96e8f4e2d92d376c6db12fac
WiKiDog W <16198436+wikidog@users.noreply.github.com>
2019-03-31 11:00:36 -0700

    20190331-01

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
   app.use(proxy('/chunksdone', { target: 'http://localhost:5000/' }));
   app.use(proxy('/upload', { target: 'http://localhost:5000/' }));
   app.use(proxy('/uploads', { target: 'http://localhost:5000/' }));
+
+  // app.use(proxy('/chunksdone', { target: 'http://uscistactrst04:5000/' }));
+  // app.use(proxy('/upload', { target: 'http://uscistactrst04:5000/' }));
+  // app.use(proxy('/uploads', { target: 'http://uscistactrst04:5000/' }));
 };
